9e25f66a288d226103531d39bdb7485f44ad19e6
baekege <geert@baeke.info>
2025-01-26 13:23:08 +0100

    add telemetry

--- tools/__init__.py
+++ tools/__init__.py
@@ -3,9 +3,14 @@ from .browser_tool import BrowserTool
 from .pdf_tool import PDFTool
 from .scrape_tool import ScrapeTool
 
+
 __all__ = [
     'BingSearchTool',
     'BrowserTool',
     'PDFTool',
     'ScrapeTool'
-] 
+]
+        
+
+
+            
